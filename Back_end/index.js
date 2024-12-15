ad75bcd0145306b00810a7a552d6af5055c06c4a
mnscmart <git@github.com:mnscmart/APP_calculadora_simples.git>
2024-12-15 18:09:53 -0300

    correçao da coneção do mongodb

--- Back_end/index.js
+++ Back_end/index.js
@@ -1,27 +1,62 @@
-//const http = require('http');
-//const server = http.createServer((req, res) => {
-//  res.statusCode = 200;
-//  res.setHeader('Content-Type', 'text/plain');
-//  res.end('Hello, World!\n');
-//});
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
 
-//server.listen(3000, () => {
-//  console.log('Servidor rodando em http://localhost:3000/');
-//});
-
-var express = require('express');
-var router = express.Router();
-var db = require('../db')
-
-/* GET index. */ 
-router.get('/', async (req, res, next) => {
-  try {
-    const funcionarios = await db.findAll();
-    res.render('index', { title: 'Funcionarios', funcionarios });
-  } catch (err) {
-    next(err);
-  }
-})
-
-module.exports = router;
-
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000/');
+ });
+ var createError = require('http-errors');lc
+ var express = require('express');
+ var path = require('path');
+ var cookieParser = require('cookie-parser');
+ var logger = require('morgan');
+ var cors = require('cors');
+ 
+ //var salasRouter = require('./routes/salas');
+ var clientesRouter = require('./routes/clientesRoute');
+ //var funcionariosRouter = require('./routes/funcionarios');
+ //var reservasRouter = require('./routes/reservas');
+ 
+ var app = express();
+ 
+ // CORS
+ app.use(cors({origin:'http://localhost:5000'}));
+ app.options('*', cors());
+ 
+ // view engine setup
+ //app.set('views', path.join(__dirname, 'views'));
+ //app.set('view engine', 'ejs');
+ 
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+ app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
+ 
+ //app.use('/salas', salasRouter);
+ app.use('/clientes', clientesRouter);
+ //app.use('/funcionarios', funcionariosRouter);
+ //app.use('/reservas', reservasRouter);
+ 
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   next(createError(404));
+ });
+ 
+ // error handler
+ app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
+ });
+ 
+ module.exports = app;
+ 
+ 
--- Back_end/models/clientesModel.js
+++ Back_end/models/clientesModel.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://aiorosdesagitario13:gVlfM6GLCB2MmJj5@cluster0.6b4io.mongodb.net/', { useNewUrlParser: true});
+// Conexão com o MongoDB sem as opções descontinuadas
+mongoose.connect('mongodb+srv://aiorosdesagitario13:gVlfM6GLCB2MmJj5@cluster0.6b4io.mongodb.net/', {
+  useNewUrlParser: true,  // Se necessário, mas apenas para versões antigas do driver
+})
+.then(() => {
+  console.log('Conectado ao MongoDB com sucesso!');
+})
+.catch((err) => {
+  console.error('Erro na conexão com o MongoDB:', err);
+});
 
 var Schema = mongoose.Schema;
 
-const cliente = new Schema ({
+// Definindo o esquema do cliente
+const cliente = new Schema({
+  cpf        : { type: Number, required: true },
+  nome       : { type: String, required: true },
+  cep        : { type: Number },
+  telefone   : { type: Number, required: true },
+  email      : { type: String, required: true },
+});
 
-    cpf        : { type:Number, required: true},
-    nome       : { type:String, required: true},
-    cep        : { type:Number },
-    telefone   : { type:Number, required: true},
-    email      : { type:String, required: true}
-})
-
+// Criando o modelo baseado no esquema
 const ClienteModel = mongoose.model('clientes', cliente);
 
-
-module.exports = {ClienteModel}
+// Exportando o modelo
+module.exports = { ClienteModel };
